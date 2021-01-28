987f11c9156131a54dc57159200cf435dd2b9ac0
kevm124 <kmurciab@purdue.edu>
2021-01-28 16:02:05 -0500

    Added interface to return enumerations of categories

---
+++ SupermarketAPI/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SupermarketAPI.Models;
+
+namespace SupermarketAPI.Services
+{
+    interface ICategoryService
+    {
+        Task<IEnumerable<Category>> ListAsync();
+    }
+}
