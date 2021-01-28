b6fe83acd65efc665593de0a5cfa76d472968547
kevm124 <kmurciab@purdue.edu>
2021-01-28 14:36:37 -0500

    Add basic database context

---
+++ SupermarketAPI/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketAPI.Models;
+
+namespace SupermarketAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Products> Products { get; set; }
+    }
+}
