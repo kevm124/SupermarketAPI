b00a5308e08a6a2b8b622c25253bde2b95ad38cc
kevm124 <kmurciab@purdue.edu>
2021-01-29 14:11:33 -0500

    Ensure in-memory databse created at application start

--- SupermarketAPI/Program.cs
+++ SupermarketAPI/Program.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SupermarketAPI.Context;
 
 namespace SupermarketAPI
 {
@@ -13,7 +15,15 @@ namespace SupermarketAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            using(var context = scope.ServiceProvider.GetService<AppDbContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
