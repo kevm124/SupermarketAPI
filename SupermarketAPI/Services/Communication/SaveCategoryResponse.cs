601605b7b1b78eac3ff88bf9bb813431f971d5bd
kevm124 <kmurciab@purdue.edu>
2021-01-30 14:45:41 -0500

    Added new response to when creating new category and added validation to post method

---
+++ SupermarketAPI/Services/Communication/SaveCategoryResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SupermarketAPI.Models;
+
+namespace SupermarketAPI.Services.Communication
+{
+    public class SaveCategoryResponse : BaseResponse
+    {
+        public Category Category { get; private set; }
+
+        private SaveCategoryResponse(bool sucess, String message, Category category) : base(sucess, message)
+        {
+            Category = category;
+        }
+
+        //Creates success response
+        public SaveCategoryResponse(Category category) : this(true, string.Empty, category)
+        {
+        }
+
+        //Creates an error response
+        public SaveCategoryResponse(string message) : this(false, message, null)
+        {
+        }
+    }
+}
--- SupermarketAPI/Services/ICategoryService.cs
+++ SupermarketAPI/Services/ICategoryService.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SupermarketAPI.Models;
+using SupermarketAPI.Services.Communication;
 
 namespace SupermarketAPI.Services
 {
     public interface ICategoryService
     {
         Task<IEnumerable<Category>> ListAsync();
+        Task<SaveCategoryResponse> SaveAsync(Category category);
     }
 }
