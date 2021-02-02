7e49108ab93f45054d844a450d5648e75c0ad09e
kevm124 <kmurciab@purdue.edu>
2021-02-02 13:17:23 -0500

    Added an extension method to Reflection API

---
+++ SupermarketAPI/Extension/EnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace SupermarketAPI.Extension
+{
+    public static class EnumExtensions
+    {
+        //Define generic method
+        public static string ToDescriptionString<TEnum>(this TEnum @enum)
+        {
+            //get type of info (class, inteface, struct, etc definition) w/ GetType
+            FieldInfo info = @enum.GetType().GetField(@enum.ToString()); //gets specific enumeration value
+            //finds all description attrubutes applied over enumeration value and stores in array
+            var attributes = (DescriptionAttribute[])info.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            //short hand to check if there is at least one not null description attribute for enumeration type
+            return attributes?[0].Description ?? @enum.ToString();
+        }
+    }
+}
