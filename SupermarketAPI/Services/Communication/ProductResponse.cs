be08af33ee671edb4785c379d959578ded80acbc
kevm124 <kmurciab@purdue.edu>
2021-02-03 12:45:41 -0500

    Added product response

---
+++ SupermarketAPI/Services/Communication/ProductResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SupermarketAPI.Models;
+
+namespace SupermarketAPI.Services.Communication
+{
+    public class ProductResponse : BaseResponse
+    {
+        public Products Product { get; private set; }
+
+        private ProductResponse(bool success, String message, Products product) : base(success, message)
+        {
+            Product = product;
+        }
+
+        //Create succes response
+        public ProductResponse(Products product) : this(true, string.Empty, product)
+        {
+        }
+
+        //Create an error response
+        public ProductResponse(string message) : this(false, message, null)
+        {
+        }
+    }
+}
